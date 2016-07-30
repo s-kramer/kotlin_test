7cda113f6a4674b81f3deb69c50ab207a2fc5cae
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-07-30 17:55:20 +0200

    list of two different elements is not handled

--- romanNumerals/src/main/kotlin/algorithm/groupAdjacent.kt
+++ romanNumerals/src/main/kotlin/algorithm/groupAdjacent.kt
@@ -6,7 +6,16 @@ package algorithm
  * groupAdjacent algorithm. Uses the provided predicate to group subsequent entries in the collection.
  */
 
-fun List<Int>.groupAdjacent(): List<Pair<Int, Int>> {
-    val list = mutableListOf((1 to size))
-    return list
+fun <E> List<E>.groupAdjacent(): List<List<E>> {
+    var result = arrayListOf<List<E>>()
+
+    var currentPrototypeIndex = 0
+
+    while (currentPrototypeIndex < this.size) {
+        val list = subList(currentPrototypeIndex, this.size).takeWhile { it == this[currentPrototypeIndex] }
+        result.add(list)
+        currentPrototypeIndex += list.size
+    }
+
+    return result
 }
--- romanNumerals/src/test/kotlin/algorithm/groupAdjacentTest.kt
+++ romanNumerals/src/test/kotlin/algorithm/groupAdjacentTest.kt
@@ -7,9 +7,9 @@ import org.junit.Test
 /**
  * Created by skramer on 7/30/16.
  * Tests for groupAdjacent algorithm.
- * - single element
- * - two equal elements
- * - two different elements
+ * - <s>single element</s>
+ * - <s>two equal elements</s>
+ * - <s>two different elements</s>
  * - only equal elements return list with a single pair
  * - all elements different return list of pairs of all elements each with 1
  * - empty list
@@ -18,17 +18,22 @@ import org.junit.Test
 class GroupAdjacentTest() {
     @Test
     fun singleElementIsPlacedInSinglePair() {
-        assertThat(listOf(1).groupAdjacent(), `is`(listOf(1 to 1)))
+        assertThat(listOf(1).groupAdjacent(), `is`(listOf(listOf(1))))
     }
 
     @Test
     fun twoEqualElementsReturnListWithOnlyOnePair() {
-        assertThat(listOf(1, 1).groupAdjacent(), `is`(listOf(1 to 2)))
+        assertThat(listOf(1, 1).groupAdjacent(), `is`(listOf(listOf(1, 1))))
     }
 
     @Test
     fun multipleEqualElementsReturnListWithOnlyOnePair() {
         val equalElements = (1 .. 100).map { 1 }.toList()
-        assertThat(equalElements.groupAdjacent(), `is`(listOf(1 to 100)))
+        assertThat(equalElements.groupAdjacent(), `is`(listOf(equalElements)))
+    }
+
+    @Test
+    fun twoDifferentElementsReturnPairEach() {
+        assertThat(listOf(1, 2).groupAdjacent(), `is`(listOf(listOf(1), listOf(2))))
     }
 }
